342eba2e502eee3275aa57e363a2a7fd118af997
Yakkala Pavankumar <Yakkala Pavankumar@YakkalaPavankum>
2020-07-13 16:59:24 +0530

    my first project

---
+++ src/githubsample/login.java
@@ -0,0 +1,10 @@
+package githubsample;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/githubsample/password.java
@@ -0,0 +1,10 @@
+package githubsample;
+
+public class password {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
